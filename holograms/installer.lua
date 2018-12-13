ba9a7f3bf4d8efde0746d6e13a565c3240c5abb1
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-13 16:43:30 +1000

    Update installer.lua

--- holograms/installer.lua
+++ holograms/installer.lua
@@ -13,12 +13,13 @@ end
 print("\nПрограмма для отображения установлена.")
 os.sleep(2)
 if not fs.exists("first_holo.holo") then
-shell.execute("wget https://github.com/BarawikS/Holo/blob/master/holograms/first_holo.holo first_holo.holo")
+shell.execute("wget https://github.com/BarawikS/Holo/blob/master/holograms/test_holo.holo test_holo.holo")
 end
 print("\nТестовая голограмма установлена.")
 -- Перезапуск компа
- print("\nШобы перезапустить компьютер, просто тыкните.")
+ print("\nЧтобы выключить инсталлер, нажмите.")
                 local e = event.pull("touch")
-                print("\nРестарт...")
+                print("\nОтключение..")
                 os.sleep(2)
-                shell.execute("reboot")
+                shell.execute("clear")
+                os.sleep(1)
