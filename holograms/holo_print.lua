0742e317cc91f5b5fc4abb875846abb1be286149
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-13 17:19:06 +1000

    Add files via upload

--- holograms/holo_print.lua
+++ holograms/holo_print.lua
@@ -4,9 +4,9 @@
 -- |  Проект: MCSkill.ru Сервер:TMSB |
 -- |  Developer: Barawik_ Modded: No |
 -- |=================================|
-component = require("component")
-holo = component.hologram
-gpu = component.gpu
+local component = require("component")
+local holo = component.hologram
+local gpu = component.gpu
 string1 = ""
 z = 24
 i = 0
